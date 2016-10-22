3df8f4b3145cda02a80fdd1b43113cdb1d3232bb
likhith123 <ae15b032@smail.iitm.ac.in>
2016-10-22 12:19:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-<div hello;>
 
-</div>
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
